c90ed065fc4e17577c8e830f58c8b2e36911528c
Nazario Cano <1026187a@umich.mx>
2023-03-26 17:49:21 -0600

    ejercicios strings listo

---
+++ index.js
@@ -0,0 +1,43 @@
+// Crea un archivo JS que contenga las siguientes líneas
+// - Una cadena de texto con tu Nombre
+// - Otra cadena de texto con tu Apellido
+// - Una cadena de texto que se llame "estudiante" concatenando tu Nombre y tu Apellido con un espacio entre medias
+// - Una cadena de texto que se llame "estudianteMayus" que contenga la cadena estudiante pero todo en mayúsculas
+// - Una cadena de texto que se llame "estudianteMinus" que contenga la cadena estudiante pero todo en minúsculas
+// - Una variable que contenga el número de letras de la cadena "estudiante" contando los espacios
+// - Una variable que contenga la primera letra del Nombre
+// - Otra variable que contenga la última letra del Apellido
+// - Una cadena de texto que elimine los espacios de la variable "estudiante"
+// - Una variable booleana que diga si el Nombre está contenido en la variable "estudiante"
+
+const nombre = "Efrain";
+const appellido = "Tovar";
+const studiante = `${nombre} ${appellido}`;
+const estudianteMayus = studiante.toUpperCase();
+console.log(estudianteMayus);
+const estudianteMinus = studiante.toLowerCase();
+console.log(estudianteMinus);
+
+const length = studiante.length;
+console.log(length);
+const primeraLetra = nombre[0];
+console.log(primeraLetra);
+const ultimaLetra = appellido[appellido.length -1];
+console.log(ultimaLetra);
+
+const eliminarEsp = studiante.trim();
+console.log(eliminarEsp);
+
+const existe = studiante.includes(nombre);
+console.log(existe);
+
+
+
+
+
+
+
+
+
+
+
